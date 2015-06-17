4ecb8f92c97038b77b707087bd5bd2bfd95b1e98
TuanNic <tuanpapd00299@gmail.com>
2015-06-17 10:58:52 +0700

    Test up code lần 2

--- TestGit/src/GD2/Main.java
+++ TestGit/src/GD2/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello Assginment giai doan 2");
+		System.out.print("Test up code");
 	}
 
 }
